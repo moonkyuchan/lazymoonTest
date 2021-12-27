962d74c672fdcfef817fe9c37d076975fc4a8b57
문규찬 <lazychan90@gmail.com>
2021-12-27 12:48:39 +0900

    firebase 초기세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
+// import firebase from "./firebase";
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
